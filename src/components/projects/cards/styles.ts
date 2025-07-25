e81f827fb5fd4955e1155edb886ead194d01e4c6
TommyOliveir <90339122+TommyOliveir@users.noreply.github.com>
2025-07-25 19:28:08 +0200

    add project component

---
+++ src/components/projects/cards/styles.ts
@@ -0,0 +1,8 @@
+import { styled } from '@mui/system'
+
+export const CardListStyled = styled('div')({
+  gap: '40px',
+  width: '50%',
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+})
--- src/components/projects/styles.ts
+++ src/components/projects/styles.ts
@@ -2,12 +2,10 @@ import { styled } from '@mui/system'
 
 export const ProjectsStyled = styled('header')((props) => ({
   color: props.theme.palette.primary.main,
-  //   backgroundColor: props.bgColor || props.theme.palette.background.default,
-  //   alignItems: 'center',
-  //   position: 'sticky',
-  //   display: 'flex',
-  //   top: 0,
-  padding: '1em',
+  // border: 'red solid 6px',
+  padding: '2em',
+  display: 'flex',
+  justifyContent: 'center',
   //   zIndex: 999,
   //   [props.theme.breakpoints.up('lg')]: {
   //     padding: '1em 20em',
